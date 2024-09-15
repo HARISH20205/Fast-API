19d47378074d6f12c12e9f16f7137f4a3615d670
HARISH20205 <harishkb20205@gmail.com>
2024-09-16 03:36:38 +0530

    query string

--- schemas.py
+++ schemas.py
@@ -1,13 +1,21 @@
 from enum import Enum
 from pydantic import BaseModel
-
+from datetime import date 
+from typing import Union
 class GenreURLChoices(Enum):
     ROCK = 'rock'
     ELECTRONIC = 'electronic'
     METAL = 'metal'
     HIP_HOP = 'hip-hop'
     
+class Album(BaseModel):
+    title:str
+    release_date : date
 class Band(BaseModel):
-    id: int
+    id:Union[str,int] 
     name: str
     genre: str
+    albums:list[Album] = [] # = [] can be used for default here it's []
+    
+    
+     
